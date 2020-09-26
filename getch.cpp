2e7f98f311993708da8470a0f0e4be2458bfa764
Junaid Ali <64123692+junaidali1@users.noreply.github.com>
2020-09-26 19:42:24 +0500

    Add files via upload

---
+++ getch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int a,b,ans;
+	cout << "Enter a : "; cin >> a;
+	cout << "Enter b : "; cin >> b;
+	
+	ans = a * b;
+	
+	cout <<ans;
+	
+}
