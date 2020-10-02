0335cd68f815bc8229ae576fec29f9cab441ff5b
Junaid Ali <64123692+junaidali1@users.noreply.github.com>
2020-10-03 00:14:10 +0500

    Swaping two numbers

---
+++ Swaping two numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int a,b,temp;
+	a = 2;b = 5;
+	
+	cout << "Before Swaping";
+	cout << "\na = "<<a;
+	cout << "\nb = "<<b;
+	
+	temp = a;
+	a = b;
+	b = temp;
+	
+	
+	
+	cout << "After Swaping";
+	cout << "\na = "<<a;
+	cout << "\nb = "<<b;
+}
